e638ac7c68192ba06f434c58c63cf4e82a5290e8
Cesar Menegatti <cesar.menegatti@outlook.com.br>
2022-06-30 00:17:36 -0300

    Server and DB Connection Done

---
+++ database/database.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func ConnectDb() {
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect do the database! \n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database successfully!")
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Running migrations")
+
+	//TODO: add migrations
+}
